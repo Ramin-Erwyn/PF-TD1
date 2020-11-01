d500806321adf913ef2ab3f1c34208f8a9f0baa6
RAMIN <r.erwyn@gmail.com>
2020-11-01 21:55:08 +0100

    .

--- src/main/java/PF1/EXO2q2/Arbre.java
+++ src/main/java/PF1/EXO2q2/Arbre.java
@@ -2,7 +2,7 @@ package PF1.EXO2q2;
 
 import java.util.Set;
 
-public interface Arbre {
+public interface Arbre<S extends Sommable<S>> {
     int taille();
     boolean contient(final Integer val);
     Set<Integer> valeurs();
